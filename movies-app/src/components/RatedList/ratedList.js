49040a836947ea7f816e69cb4114ac2b573753f2
Akhvatov414 <superbus414@gmail.com>
2023-02-04 17:05:45 +0300

    minor fixes
    Перенос между машинами

--- movies-app/src/components/RatedList/ratedList.js
+++ movies-app/src/components/RatedList/ratedList.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import MovieService from '../../services/movie-service';
 
 class RatedList extends Component {
+    movieService = new MovieService();
+    constructor() {
+        super();
+
+        this.state = {
+            ratedItems: [],
+            isLoading: true,
+            page: 1,
+            totalResults: 0,
+        };
+    }
+
+    componentDidMount() {
+        this.getRatedItems(1);
+    }
+
+    getRatedItems = (page) => {
+        this.movieService.getRatedMovies(page).then((data) => {
+            console.log(data);
+        })
+    }
     render() {
         return (
             <div>
