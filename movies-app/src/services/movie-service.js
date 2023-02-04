ac20180df3be8897568c4e10ca6e7740e7d0bc7d
Akhvatov414 <superbus414@gmail.com>
2023-02-04 16:35:21 +0300

    minor fixes
    test

--- movies-app/src/services/movie-service.js
+++ movies-app/src/services/movie-service.js
@@ -66,6 +66,7 @@ export default class MovieService {
   async rateMovie(id, rate) {
     try {
       const sessionId = localStorage.getItem('guestSessionId');
+      console.log(sessionId);
       const queryUrl = `${this._apiURL}${id}/rating${this._apiKey}&guest_session_id=${sessionId}`;
       const res = await fetch(queryUrl, {
         method: 'POST',
